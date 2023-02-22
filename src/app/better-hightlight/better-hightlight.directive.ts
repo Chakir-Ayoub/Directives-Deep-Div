9b2a222ba115b8a78cf029de9d0b7edc587663fb
Chakir Ayoub <ayoub.chakir0926@gmail.com>
2023-02-22 21:10:03 +0100

    HostBinding

--- src/app/better-hightlight/better-hightlight.directive.ts
+++ src/app/better-hightlight/better-hightlight.directive.ts
@@ -1,20 +1,23 @@
-import { Directive, ElementRef, Renderer2,HostListener, OnInit  } from '@angular/core';
+import { Directive, ElementRef, Renderer2,HostListener, OnInit, HostBinding  } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHightlight]'
 })
 export class BetterHightlightDirective implements OnInit {
+  @HostBinding('style.backgroundColor') background: string='red';
 
   constructor(private elRef:ElementRef,private rendrer:Renderer2) { }
   ngOnInit() {
   //  this.rendrer.setStyle(this.elRef.nativeElement,'background-color','blue');
       }
     @HostListener('mouseentr') mouseover(event:Event){
-      this.rendrer.setStyle(this.elRef.nativeElement,'background-color','blue');
+     // this.rendrer.setStyle(this.elRef.nativeElement,'background-color','blue');
+        this.background='black';
     }
 
     @HostListener('mouseleave') mouseleave(event:Event){
-      this.rendrer.setStyle(this.elRef.nativeElement,'background-color','red');
+      // this.rendrer.setStyle(this.elRef.nativeElement,'background-color','red');
+        this.background='red';
     }
 
 }
